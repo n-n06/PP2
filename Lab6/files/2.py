06ef1f36e8ec19d4784775bf60d7e3648fc51288
nurs <nursultanzhantuar@gmail.com>
2024-02-06 16:49:40 +0600

    deleted unrelated files

--- Lab6/files/2.py
+++
@@ -1,9 +0,0 @@
-def tree(n):
-    for i in range(1, n + 1):
-        print(" "*(n - i), end = "")
-        for j in range(1 + (2 * (i - 1))):
-            print("*", end = "")
-        print(" "*(n - i))
-
-n = int(input())
-tree(n)
