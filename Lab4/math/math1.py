5e3df1587e0fef1f3b7c56f267a319d61fc1ae2e
nurs <nursultanzhantuar@gmail.com>
2024-02-20 16:42:48 +0600

    fixed math1

--- Lab4/math/math1.py
+++ Lab4/math/math1.py
@@ -1,4 +1,9 @@
 import math
 
+
 deg = float(input("Input degree: "))
 print("Output radian: {:.6f}".format(math.radians(deg)))
+
+
+rad = (deg * math.pi) / 180.0
+print("Output radian: {:.6f}".format(rad))
